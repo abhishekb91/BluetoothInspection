62c6e3581a8e1f5c298ea29a56b1d8e202e0ec91
Abhishek Bhandari <abhishekbhandari1991@gmail.com>
2017-01-22 14:53:45 -0500

    [+] Added Configuration Activity

---
+++ app/src/main/java/com/wpi/walkermagnet_inspection/activity/ConfigurationActivity.java
@@ -0,0 +1,15 @@
+package com.wpi.walkermagnet_inspection.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wpi.walkermagnet_inspection.R;
+
+public class ConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+    }
+}
